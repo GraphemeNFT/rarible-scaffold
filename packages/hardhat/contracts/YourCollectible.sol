b0a573f2814b24a6134aa77d3ed4d5d46a1c41f1
Srivatsa Sharma <srivatsa.sharma@keysight.com>
2021-08-24 20:53:42 +0530

    added some contract functions

--- packages/hardhat/contracts/YourCollectible.sol
+++ packages/hardhat/contracts/YourCollectible.sol
@@ -1,47 +1,105 @@
 pragma solidity 0.7.5;
+//  pragma solidity ^0.8.0;
+// pragma abicoder v2;
 //SPDX-License-Identifier: MIT
 
 //import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-//learn more: https://docs.openzeppelin.com/contracts/3.x/erc721
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 // GET LISTED ON OPENSEA: https://testnets.opensea.io/get-listed/step-two
 
 contract YourCollectible is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-  using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
+    string customBaseUri = "http://website.com/id/";
+    mapping(uint256 => ItemDetail) private _items;
+    mapping(uint256 => ItemPosition[]) private _itemPositions;
 
-  constructor() public ERC721("YourCollectible", "YCB") {
-    _setBaseURI("https://ipfs.io/ipfs/");
-  }
+    struct ItemDetail {
+        bool isPrimitive;
+        uint256 identifier;
+    }
 
-  function mintItem(address to, string memory tokenURI)
-      public
-      returns (uint256)
-  {
-      _tokenIds.increment();
+    struct ItemPosition {
+        uint256 tokenId;
+        uint256 row;
+        uint256 column;
+    }
 
-      uint256 id = _tokenIds.current();
-      _mint(to, id);
-      _setTokenURI(id, tokenURI);
+    constructor() public ERC721("YourCollectible", "YCB") {
+        _setBaseURI("https://ipfs.io/ipfs/");
+    }
 
-      return id;
-  }
+    function mintItem(address to, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
 
-   mapping(uint256 => uint256[]) private _children;
+        uint256 id = _tokenIds.current();
+        _mint(to, id);
+        _setTokenURI(id, tokenURI);
 
-   function assignChildren(uint256 tokenId, uint256[] memory childIds) public {
-      require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not owner nor approved");
-      // need a check if the children exist or not
-      _children[tokenId] = childIds;
-   }
+        return id;
+    }
 
-   function getChildren(uint256 tokenId) 
-   public view
-    returns (uint256[] memory) {
-       require(_exists(tokenId), "ERC721: Nonexistent token");
-      return _children[tokenId];
-   }
+    function mintCharacterToken(address to, uint256 identifier)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 id = _tokenIds.current();
+        _mint(to, id);
+        _setTokenURI(
+            id,
+            string(abi.encodePacked(customBaseUri, Strings.toString(id)))
+        );
+        _items[id] = ItemDetail({isPrimitive: true, identifier: identifier});
+        return id;
+    }
+
+    /*
+    // Fails as it will not accept struct[] as argument. Need to figure how to set this.
+    function mintComplexCharacterToken(
+        address to,
+        ItemPosition[] memory itemPositions
+    ) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 id = _tokenIds.current();
+        _mint(to, id);
+        _setTokenURI(
+            id,
+            string(abi.encodePacked(customBaseUri, Strings.toString(id)))
+        );
+        _items[id] = ItemDetail({isPrimitive: false, identifier: _identifier});
+        _itemPositions[id] = itemPositions;
+        return id;
+    }
+    */
+
+    function getLetterDNA(uint256 tokenId) public view returns (uint256) {
+        return _items[tokenId].identifier;
+    }
+
+    function rollDiceToMint(address to) public payable {
+        // should mint 'n' tokens.
+        // should some funds go to DAO/Owner as creation fee??
+        uint256 i = 0;
+        uint256 prc = getRandomness();
+        for (i = 0; i <= 5; i++) {
+            //  uint random = uint(keccak256(prc + i));
+            uint256 random = uint256(prc);
+            mintCharacterToken(to, random);
+        }
+    }
+
+    function getRandomness() private returns (uint256) {
+        // do this as a temp measure. Investigate VRF options
+        return uint256(blockhash(block.number));
+    }
 }
