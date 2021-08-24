737e85122b9916424fd421be8f7455b08776b08d
Srivatsa Sharma <srivatsa.sharma@keysight.com>
2021-08-24 21:51:49 +0530

    misc

--- packages/hardhat/contracts/YourCollectible.sol
+++ packages/hardhat/contracts/YourCollectible.sol
@@ -1,5 +1,5 @@
 pragma solidity 0.7.5;
-//  pragma solidity ^0.8.0;
+// pragma solidity ^0.8.0;
 // pragma abicoder v2;
 //SPDX-License-Identifier: MIT
 
@@ -14,7 +14,6 @@ contract YourCollectible is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    string customBaseUri = "http://website.com/id/";
     mapping(uint256 => ItemDetail) private _items;
     mapping(uint256 => ItemPosition[]) private _itemPositions;
 
@@ -34,7 +33,7 @@ contract YourCollectible is ERC721 {
     }
 
     function mintItem(address to, string memory tokenURI)
-        public
+        private
         returns (uint256)
     {
         _tokenIds.increment();
@@ -47,17 +46,13 @@ contract YourCollectible is ERC721 {
     }
 
     function mintCharacterToken(address to, uint256 identifier)
-        public
+        private
         returns (uint256)
     {
         _tokenIds.increment();
 
         uint256 id = _tokenIds.current();
         _mint(to, id);
-        _setTokenURI(
-            id,
-            string(abi.encodePacked(customBaseUri, Strings.toString(id)))
-        );
         _items[id] = ItemDetail({isPrimitive: true, identifier: identifier});
         return id;
     }
@@ -82,24 +77,24 @@ contract YourCollectible is ERC721 {
     }
     */
 
-    function getLetterDNA(uint256 tokenId) public view returns (uint256) {
+    function getCharacterIdentifier(uint256 tokenId) public view returns (uint256) {
         return _items[tokenId].identifier;
     }
 
-    function rollDiceToMint(address to) public payable {
+    function rollToMint(address to) public payable {
         // should mint 'n' tokens.
         // should some funds go to DAO/Owner as creation fee??
         uint256 i = 0;
         uint256 prc = getRandomness();
         for (i = 0; i <= 5; i++) {
-            //  uint random = uint(keccak256(prc + i));
-            uint256 random = uint256(prc);
+             uint256 random = uint256(keccak256(abi.encodePacked(prc, i)));
+            // uint256 random = uint256(prc);
             mintCharacterToken(to, random);
         }
     }
 
-    function getRandomness() private returns (uint256) {
+    function getRandomness() private view returns (uint256) {
         // do this as a temp measure. Investigate VRF options
-        return uint256(blockhash(block.number));
+        return uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, blockhash(block.number))));
     }
 }
