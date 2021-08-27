9cff750d3164407068c7051a01304203f72507da
Srivatsa Sharma <srivatsa.sharma@keysight.com>
2021-08-27 13:54:19 +0530

    contract updates

--- packages/hardhat/test/GraphemeTest.js
+++ packages/hardhat/test/GraphemeTest.js
@@ -11,13 +11,14 @@ describe("Grapheme Tests", function () {
     let owner;
     let addr1;
     let addr2;
+    let addr3;
     let addrs;
 
     describe("Contact stuff", function () {
         it("should deploy contract", async function () {
             const Grapheme = await ethers.getContractFactory("Grapheme");
             graphemeInstance = await Grapheme.deploy();
-            [owner, addr1, addr2, _] = await ethers.getSigners();
+            [owner, addr1, addr2, addr3, _] = await ethers.getSigners();
         });
     });
 
@@ -69,16 +70,56 @@ describe("Grapheme Tests", function () {
             it("Should emit relevant events", async function () {
                 await expect(graphemeInstance.rollToMint(addr2.address))
                     .to.emit(graphemeInstance, 'Transfer')
-                    .withArgs(function () {}, addr2.address, 7)
-                    .withArgs(function () {}, addr2.address, 8)
-                    .withArgs(function () {}, addr2.address, 9)
-                    .withArgs(function () {}, addr2.address, 10)
-                    .withArgs(function () {}, addr2.address, 11)
-                    .withArgs(function () {}, addr2.address, 12)
+                    .withArgs(function () { }, addr2.address, 7)
+                    .withArgs(function () { }, addr2.address, 8)
+                    .withArgs(function () { }, addr2.address, 9)
+                    .withArgs(function () { }, addr2.address, 10)
+                    .withArgs(function () { }, addr2.address, 11)
+                    .withArgs(function () { }, addr2.address, 12)
                     .to.emit(graphemeInstance, 'Roll')
-                    .withArgs(addr2.address, function () {});
+                    .withArgs(addr2.address, function () { });
             });
 
+        });
+
+        describe("Mint word", function () {
+            it("Should mint a word", async function () {
+                expect(await graphemeInstance.balanceOf(addr3.address)).to.equal(0);
+
+                let tokenIds = [1, 2, 3];
+                let rows = [4,5,6];
+                let cols = [7,8,9];
+                let tokenUri = 'testUrl1';
+                let receipt = await graphemeInstance.connect(addr3).mintWord(addr3.address, tokenUri, tokenIds, rows, cols);
+                expect(await graphemeInstance.balanceOf(addr3.address)).to.equal(1);
+
+                expect(await graphemeInstance.balanceOf(addr3.address)).to.equal(1);
+
+                let resWord = await graphemeInstance.GetWord(await graphemeInstance.totalSupply());
+                // console.log('resWord',resWord);
+                assert.notDeepEqual(tokenIds, [1,1,1]); // test false
+                assert.deepEqual(resWord[0].map(a => a.toNumber()), tokenIds);
+                assert.deepEqual(resWord[1].map(a => a.toNumber()), rows);
+                assert.deepEqual(resWord[2].map(a => a.toNumber()), cols);
+            });
+
+            it("Save and get word", async function () {
+                let tokenIds = [1, 2, 3];
+                let rows = [4,5,6];
+                let cols = [7,8,9];
+                let tokenUri = 'testUrl1';
+                await graphemeInstance.mintWord(addr3.address, tokenUri, tokenIds, rows, cols);
+                cols = [2,2,2];
+                let tokenId = await graphemeInstance.totalSupply();
+                
+                await graphemeInstance.connect(addr3).saveWord(tokenId, tokenUri, tokenIds, rows, cols);
+
+                let resWord = await graphemeInstance.GetWord(tokenId);
+                assert.notDeepEqual(tokenIds, [1,1,1]); // test false
+                assert.deepEqual(resWord[0].map(a => a.toNumber()), tokenIds);
+                assert.deepEqual(resWord[1].map(a => a.toNumber()), rows);
+                assert.deepEqual(resWord[2].map(a => a.toNumber()), cols);
+            });
         });
 
     });
@@ -92,5 +133,5 @@ describe("Grapheme Tests", function () {
             expect(await graphemeInstance.tokenURI(1)).to.contain('testUri1');
         });
     });
-    
+
 });
