053c91835068266ef12a06ed640af4c4e54ea408
dcsan <dc@rikai.co>
2021-08-28 22:27:06 -0700

    added state with Zustand closes #61

--- packages/react-app/src/components/Letters/Letters.jsx
+++ packages/react-app/src/components/Letters/Letters.jsx
@@ -1,5 +1,6 @@
-import React from "react"; // useCallback, useEffect, useState
-import { StaticJsonRpcProvider, Web3Provider } from "@ethersproject/providers";
+import React, { useState } from "react"; // useCallback, useEffect, useState
+// import { StaticJsonRpcProvider, Web3Provider } from "@ethersproject/providers";
+import create from 'zustand'
 
 import Mint from '../Mint/Mint'
 import OneLetter from './OneLetter'
@@ -8,9 +9,12 @@ import './letters.css'
 // const scaffoldEthProvider = null; // new StaticJsonRpcProvider("https://rpc.scaffoldeth.io:48544");
 // const mainnetInfura = new StaticJsonRpcProvider("https://mainnet.infura.io/v3/" + INFURA_ID);
 
+import useStore from '../../helpers/Store'
 
 export default function Letters (props) {
     // console.log('letters.props', props.dataSource)
+    // const wallet = useStore(state => state.wallet)
+    const localWallet = useStore(state => state.wallet);
 
     const inner = (letters) => {
         return letters.map(item => {
